d0135383fafcd63aa9cfed1b50f0afd2c4d67799
elemenofi <manuel.acobo@gmail.com>
2019-04-14 20:48:02 +0200

    whitespace

--- src/Midi.tsx
+++ src/Midi.tsx
@@ -31,9 +31,11 @@ export class Midi {
 
   onMIDIMessage(event) {
     var str = "MIDI message received at timestamp " + event.timestamp + "[" + event.data.length + " bytes]: ";
+
     for (var i=0; i<event.data.length; i++) {
       str += "0x" + event.data[i].toString(16) + " ";
     }
+    
     console.log(str);
   }
 }
